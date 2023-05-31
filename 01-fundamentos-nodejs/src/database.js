826b4d1a89e1a86348dc46e8797a4d7d1c46f47b
gnunesdev <gnunes.programmer@gmail.com>
2023-05-31 16:18:44 -0300

    feat(01-fundamentos): adds delete route

--- 01-fundamentos-nodejs/src/database.js
+++ 01-fundamentos-nodejs/src/database.js
@@ -36,4 +36,15 @@ export class Database {
 
     return data;
   }
+
+  delete(table, id) {
+    const rowIndex = this.#database[table].findIndex((row) => row.id === id);
+    console.log({ table, id });
+
+    if (rowIndex > -1) {
+      console.log(2);
+      this.#database[table].splice(rowIndex, 1);
+      this.#persist();
+    }
+  }
 }
