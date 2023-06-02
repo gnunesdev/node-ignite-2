5e36c7228bf4c35c02760480a0a030bd5006ab9d
gnunesdev <gnunes.programmer@gmail.com>
2023-06-02 14:48:14 -0300

    feat(02-api-rest-nodejs): finished knex setup, adds 2 first migrations

--- 02-api-rest-nodejs/src/server.ts
+++ 02-api-rest-nodejs/src/server.ts
@@ -1,12 +1,13 @@
 import fastify from 'fastify';
+import crypto from 'node:crypto';
 import { knex } from './database';
 
 const app = fastify();
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*');
+  const transaction = await knex('transactions').select('*').where('amount', 5);
 
-  return tables;
+  return transaction;
 });
 
 app.listen({ port: 3333 }).then(() => {
